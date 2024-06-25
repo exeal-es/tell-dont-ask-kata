2520f523ac9b55ac3d1f54342550dfa04d790669
Pedro Pardal <ppardalj@gmail.com>
2024-06-25 20:10:29 +0200

    java

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
@@ -1,8 +1,13 @@
 package it.gabrieletondi.telldontaskkata.domain;
 
+import it.gabrieletondi.telldontaskkata.useCase.*;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.math.BigDecimal.valueOf;
+import static java.math.RoundingMode.HALF_UP;
 
 public class Order {
     private BigDecimal total = new BigDecimal("0.00");
@@ -12,40 +17,53 @@ public class Order {
     private OrderStatus status = OrderStatus.CREATED;
     private int id;
 
-    public void addOrderItem(OrderItem orderItem) {
+    public void approveOrReject(boolean approved) {
+        if (status.equals(OrderStatus.SHIPPED)) {
+            throw new ShippedOrdersCannotBeChangedException();
+        }
+
+        if (approved && status.equals(OrderStatus.REJECTED)) {
+            throw new RejectedOrderCannotBeApprovedException();
+        }
+
+        if (!approved && status.equals(OrderStatus.APPROVED)) {
+            throw new ApprovedOrderCannotBeRejectedException();
+        }
+
+        this.status = approved ? OrderStatus.APPROVED : OrderStatus.REJECTED;
+    }
+
+    public void addItem(SellItemRequest itemRequest, Product product) {
+        final BigDecimal unitaryTax = product.getPrice().divide(valueOf(100)).multiply(product.getCategory().getTaxPercentage()).setScale(2, HALF_UP);
+        final BigDecimal unitaryTaxedAmount = product.getPrice().add(unitaryTax).setScale(2, HALF_UP);
+        final BigDecimal taxedAmount = unitaryTaxedAmount.multiply(BigDecimal.valueOf(itemRequest.getQuantity())).setScale(2, HALF_UP);
+        final BigDecimal taxAmount = unitaryTax.multiply(BigDecimal.valueOf(itemRequest.getQuantity()));
+
+        final OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(product);
+        orderItem.setQuantity(itemRequest.getQuantity());
+        orderItem.setTax(taxAmount);
+        orderItem.setTaxedAmount(taxedAmount);
         items.add(orderItem);
+
+        this.total = getTotal().add(taxedAmount);
+        this.tax = getTax().add(taxAmount);
     }
 
     public BigDecimal getTotal() {
         return total;
     }
 
-    public void setTotal(BigDecimal total) {
-        this.total = total;
-    }
-
     public String getCurrency() {
         return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
     }
 
     public List<OrderItem> getItems() {
         return items;
     }
 
-    public void setItems(List<OrderItem> items) {
-        this.items = items;
-    }
-
     public BigDecimal getTax() {
         return tax;
-    }
-
-    public void setTax(BigDecimal tax) {
-        this.tax = tax;
     }
 
     public OrderStatus getStatus() {
